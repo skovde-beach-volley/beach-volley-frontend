84df06b1d416997119820dae06867ed74bdf20d9
sannaasp <sanna.asp@hotmail.com>
2024-06-04 10:59:25 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -24,20 +24,29 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue(), VueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   css: {
+//     preprocessorOptions: {
+//       css: {
+//         additionalData: `
+//           @import "./src/tailwind.css";
+//         `
+//       }
+//     }
+//   }
+// })
+
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': '/src'
     }
   },
-  css: {
-    preprocessorOptions: {
-      css: {
-        additionalData: `
-          @import "./src/tailwind.css";
-        `
-      }
-    }
-  }
+  plugins: [vue()]
 })
